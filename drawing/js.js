ab145cbdd4cde5bc269b8291480a5a5f13626aba
Sugar <sugarypolecat@gmail.com>
2022-03-21 19:18:57 +0100

    (PATCH) updating the website?

--- drawing/js.js
+++ drawing/js.js
@@ -1,8 +1,13 @@
-let fs;
+// initialize some reusables
+// fs is not needed casue we are not doing express stuff. Yet. Might in the future
+// let fs;
 let fiel;
 let idea;
 let predefined;
 let ul;
+
+// document start basically.
+// i know im using it on chrome so... i dont need to check "derp is this old ie derp"
 (async function start() {
 	const but = document.getElementById("generate");
 	const history = document.getElementById("clear-history");
@@ -12,6 +17,8 @@ let ul;
 	predefined = document.getElementById("predefined");
 	// fiel = require('./defined_ideas.json');
 	fiel = await (await fetch('./defined_ideas.json')).json();
+	// had to convert from promise to an async/await to resovle the response instantly. 
+
 	// .then(res => {
 	// 	// fiel = Promise.resolve(res.json());
 	// 	fiel = res.json();
@@ -26,17 +33,21 @@ let ul;
 	ul = document.getElementById("history-list");
 })();
 function generate() {
-	console.log('shite');
-	if (predefined.checked == true) {
-		console.log('checked');
-	}
-	console.log(fiel.Ideas);
+	// console.log('shite');
+	// if (predefined.checked == true) {
+	// 	console.log('checked');
+	// }
+	// console.log(fiel.Ideas);
+
+	// create new predefined idea
 	const ideas = fiel.Ideas;
 	const randomIdea = arrRan(ideas);
 	idea.innerHTML = randomIdea;
 	const li = document.createElement("li");
 	li.appendChild(document.createTextNode(randomIdea));
 	ul.appendChild(li);
+
+	// to create a smooth transition, we need to create 2 texts. One that will be overriden... orrrrrrr....... orrr....
 }
 function clearHistory() {
 	console.log('clearing');
