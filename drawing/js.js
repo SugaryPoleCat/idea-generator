31a20858e1b2b97edcc5602fa1c2c905c0c5dfb3
Sugar <sugarypolecat@gmail.com>
2022-03-23 10:16:12 +0100

    UPDATE Unique ideas I added and fixed unique idea counter It also works now with history Took a while to fix but got it Now have to figure out, what themes to do and how. And add more options.

--- drawing/js.js
+++ drawing/js.js
@@ -26,13 +26,21 @@ let predefined,
 // list
 let ul;
 
+// themeing
+let oThemeToggle,
+	oThemeWholesome,
+	themeingOptions;
+
 // random stuff
 let ideaCounter,
 	ideaCount,
 	currentIdeaCounter,
 	curIdeaCount,
-	unIdeaCount;
+	unIdeaCount,
+	unIdeaCurrCount;
 const uniqueIdeas = [];
+let uniqueIdeasCurr = [];
+let historyUniqueToggle;
 
 // document start basically.
 // i know im using it on chrome so... i dont need to check "derp is this old ie derp"
@@ -85,19 +93,25 @@ const uniqueIdeas = [];
 		charactersIdeas = fiel.characters;
 		console.log(fiel);
 
+		// themeing
+		oThemeToggle = document.getElementById("oThemeToggle");
+		oThemeToggle.addEventListener("change", themeToggler);
+		oThemeWholesome = document.getElementById("oThemeWholesome");
+		themeingOptions = document.getElementById("themeing");
+
 		// random stuff
 		ideaCounter = 0;
 		currentIdeaCounter = 0;
 		ideaCount = document.getElementById("ideaCount");
 		curIdeaCount = document.getElementById("curIdeaCount");
 		unIdeaCount = document.getElementById("unIdeaCount");
+		historyUniqueToggle = document.getElementById("historyUniqueToggle");
+		unIdeaCurrCount = document.getElementById("unIdeaCurrCount");
 
 		// run generation
 		generate();
-	}
-	catch (e) {
-		generator_stuff.innerHTML = "Site broke. Check: <br/>" + e;
-		// alert('Site broke. Check: \n' + e);
+	} catch (e) {
+		return errorPrint(e);
 	}
 })();
 function generate() {
@@ -119,8 +133,6 @@ function generate() {
 				prompt2 = arrRan(prompts);
 			} while (prompt2 == prompt1);
 		}
-		console.log(prompt1);
-		console.log(prompt2);
 
 		// get random ideas based on chosen options.
 		randomIdea = arrRan(preDefIdeas);
@@ -153,41 +165,81 @@ function generate() {
 			unIdeaCount.textContent = uniqueIdeas.length;
 		}
 
+		// create new list element and add it to the history list
+		// had to move adding to current unique list into same as adding new element.
+		// otehrwise it had issues.
+		const li = document.createElement("li");
+		if (historyUniqueToggle.checked == true && !uniqueIdeasCurr.includes(generatedIdea)) {
+			// console.log('it does not iunclude');
+			uniqueIdeasCurr.push(generatedIdea);
+			unIdeaCurrCount.textContent = uniqueIdeasCurr.length;
+			li.appendChild(document.createTextNode(generatedIdea));
+			li.classList.add("list-group-item");
+			ul.appendChild(li);
+		} else if (historyUniqueToggle.checked == false) {
+			switch (uniqueIdeasCurr.includes(generatedIdea)) {
+				case false:
+					uniqueIdeasCurr.push(generatedIdea);
+					unIdeaCurrCount.textContent = uniqueIdeasCurr.length;
+					break;
+				default:
+					break;
+			}
+			li.appendChild(document.createTextNode(generatedIdea));
+			li.classList.add("list-group-item");
+			ul.appendChild(li);
+		}
+
 		// print result
 		idea.innerHTML = generatedIdea;
 
-		// create new list element and add it to the history list
-		const li = document.createElement("li");
-		li.appendChild(document.createTextNode(randomIdea));
-		li.classList.add("list-group-item");
-		ul.appendChild(li);
-
 		// count ideas up
 		ideaCounter++;
-		currentIdeaCounter++;
+		currentIdeaCounter = ul.children.length;
 		ideaCount.textContent = ideaCounter;
 		curIdeaCount.textContent = currentIdeaCounter;
 		return;
-	}
-	catch (e) {
-		generator_stuff.innerHTML = "stuff broke. Check: <br/>" + e;
+	} catch (e) {
+		return errorPrint(e);
 	}
 }
 function clearHistory() {
 	try {
 		console.log('clearing history');
+
 		while (ul.firstChild) {
 			ul.removeChild(ul.lastChild);
 		}
 		currentIdeaCounter = 0;
 		curIdeaCount.textContent = currentIdeaCounter;
-		// alert('History cleared');
+		uniqueIdeasCurr = [];
+		unIdeaCurrCount.textContent = uniqueIdeasCurr.length;
+
 		console.log('cleared');
-	}
-	catch (e) {
-		generator_stuff.innerHTML = "stuff broke. Check: <br/>" + e;
+	} catch (e) {
+		return errorPrint(e);
 	}
 }
 function arrRan(arr) {
 	return arr[Math.floor(Math.random() * arr.length)];
+}
+function themeToggler() {
+	try {
+		switch (this.checked) {
+			case true:
+				themeingOptions.style.display = "block";
+				oThemeWholesome.disabled = false;
+				break;
+			default:
+				themeingOptions.style.display = "none";
+				oThemeWholesome.disabled = true;
+				break;
+		}
+	} catch (e) {
+		return errorPrint(e);
+	}
+}
+function errorPrint(e) {
+	generator_stuff.innerHTML = "stuff broke.<br/>Check:<br/>" + e;
+	return;
 }
